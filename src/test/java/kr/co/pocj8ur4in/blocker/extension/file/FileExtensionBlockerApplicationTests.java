b018084f36947c8be2c9137382a6332bd6ba4334
pocj8ur4in <pocj8ur4in@haii.io>
2025-12-06 21:27:40 +0900

    feat: project layout

---
+++ src/test/java/kr/co/pocj8ur4in/blocker/extension/file/FileExtensionBlockerApplicationTests.java
@@ -0,0 +1,11 @@
+package kr.co.pocj8ur4in.blocker.extension.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileExtensionBlockerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
