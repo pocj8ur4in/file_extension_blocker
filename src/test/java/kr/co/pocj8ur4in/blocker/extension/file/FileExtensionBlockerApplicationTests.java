dd3a204d5ae2c48fdcdb6eeff67e14976d20e645
pocj8ur4in <pocj8ur4in@haii.io>
2025-12-07 15:29:14 +0900

    feat: apply dependency for postgres

--- src/test/java/kr/co/pocj8ur4in/blocker/extension/file/FileExtensionBlockerApplicationTests.java
+++ src/test/java/kr/co/pocj8ur4in/blocker/extension/file/FileExtensionBlockerApplicationTests.java
@@ -1,9 +1,12 @@
 package kr.co.pocj8ur4in.blocker.extension.file;
 
+import kr.co.pocj8ur4in.blocker.extension.file.config.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfig.class)
 class FileExtensionBlockerApplicationTests {
 
     @Test
