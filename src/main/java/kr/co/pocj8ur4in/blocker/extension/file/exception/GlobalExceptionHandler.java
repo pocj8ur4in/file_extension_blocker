e286a3bad282aba3e6df9d715f8a6ca95933006d
pocj8ur4in <pocj8ur4in@gmail.com>
2025-12-07 20:28:23 +0900

    feat: FileExtensionService

--- src/main/java/kr/co/pocj8ur4in/blocker/extension/file/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/pocj8ur4in/blocker/extension/file/exception/GlobalExceptionHandler.java
@@ -1,11 +1,29 @@
 package kr.co.pocj8ur4in.blocker.extension.file.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateExtensionException.class)
+    public ResponseEntity<String> handleDuplicateExtension(DuplicateExtensionException error) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error.getMessage());
+    }
+
+    @ExceptionHandler(ExistDefaultExtensionException.class)
+    public ResponseEntity<String> handleExistDefaultExtension(
+            ExistDefaultExtensionException error) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error.getMessage());
+    }
+
+    @ExceptionHandler(ExtensionNotFoundException.class)
+    public ResponseEntity<String> handleExtensionNotFound(ExtensionNotFoundException error) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException error) {
         return ResponseEntity.internalServerError().body(error.getMessage());
--- src/main/java/kr/co/pocj8ur4in/blocker/extension/file/repository/DefaultFileExtensionRepository.java
+++ src/main/java/kr/co/pocj8ur4in/blocker/extension/file/repository/DefaultFileExtensionRepository.java
@@ -1,10 +1,17 @@
 package kr.co.pocj8ur4in.blocker.extension.file.repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import kr.co.pocj8ur4in.blocker.extension.file.entity.DefaultFileExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DefaultFileExtensionRepository extends JpaRepository<DefaultFileExtension, Long> {
+    Optional<DefaultFileExtension> findByUuid(UUID uuid);
+
     boolean existsByNameIgnoreCase(String name);
+
+    List<DefaultFileExtension> findAllByOrderByCreatedAtAsc();
 }
